6c76f51cffffaecc0538c644786e918a096a1565
Blueproducciones <57720491+Blueproducciones@users.noreply.github.com>
2019-11-16 17:12:50 -0300

    Dr.Rottoli
    Estudio juridico del Dr. Rottoli

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//SCROLL ARROW
+
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
